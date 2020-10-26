d379dbd2c296fc848cc036db904f3a19afeda05d
Loris Cuntreri <62082748+MarelGuy@users.noreply.github.com>
2020-10-26 11:46:52 -0300

    first update

---
+++ Build Week 1 - Assets/info.js
@@ -0,0 +1,19 @@
+/* 
+    Build Week 1 - Spotify Interface
+    
+    The goal is to create a copy of the Spotify Interface.
+
+    The required pages are:
+    - Login page
+    - Home page
+    - Artist page
+    - Album page
+
+    Hints:
+    > Organize your styles in order to have re-usable classes amongst the different pages
+    > Divide the tasks in a super clear way
+    > Finish the single page before moving to the next one
+    > Keep an "always on but muted" channel so that if you need help, you can just open the mic and speak out loud
+    > Commit and merge often, work on separate branches.
+
+*/
