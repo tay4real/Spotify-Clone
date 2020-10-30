c010a1265a01bd63017309128306e92d66d13b92
Ademuyiwa Otubusin <tay4real@gmail.com>
2020-10-30 14:56:46 +0100

    Update

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
+const user = {
+  name: "",
+  address: "",
+  username: "",
+  password: "",
+};
+
+const users = [];
+
 window.onload = function () {
   window.location.href = "./loginPage/index.html";
 };
